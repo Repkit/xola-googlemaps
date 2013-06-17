9b33302d45506ae8a9fb871ff80378f76d8fc2b2
Rushi Vishavadia <rushi.v@gmail.com>
2013-06-17 16:14:07 +0400

    Smoother Animations

--- js/app/views/explore.js
+++ js/app/views/explore.js
@@ -18,53 +18,56 @@ var app = app || {};
             this.medias = this.experience.get('medias');
             this.className = this.experience.get('id');
             this.eiv = new app.ExperienceImageView({el: $('#large_img')}); // initialize
-            
+
             return this.render();
         },
 
         render: function() {
             this.$el.html(this.wrapper({className: this.className}));
-            
+
             var _tmp_class = "." + this.className;
             this.$explore = this.$(_tmp_class + ' .explore_btn');
             this.$explore_panel = this.$(_tmp_class+ ' .explore_panel');
             this.load_photos(); // Start pre-loading photos
-            
+
             return this;
         },
 
         open_panel: function(e) {
             var _this = this;
-            
-            this.$explore_panel.animate({height: "240px"});
-            this.$explore.hide();
+
+            this.show_photos();
+
+            this.$explore.animate({bottom: '240px'}, {duration: 800, queue: false});
+            this.$explore_panel.animate({bottom: "0px", opacity: 1}, {duration: 800, queue: false});
 
             this.$explore.addClass('open').removeClass('closed');
             this.$explore_panel.addClass('open').removeClass('closed');
 
-            this.$explore.css('bottom', 240);
             this.$explore.fadeIn();
 
-            this.show_photos();
             setTimeout(function() {
                 _this.eiv.resize();
             }, 1000);
-            
+
             ga('send', { 'hitType': 'event', 'eventCategory': 'panel', 'eventAction': 'open', 'eventLabel': this.experience.get('name') });
         },
 
         close_panel: function(e) {
+            var _this = this;
+
             this.$explore.removeClass('open').addClass('closed');
-            this.$explore_panel.empty();
+            this.$explore.animate({bottom: "25px"}, {duration: 800, queue: false});
+
             this.$explore_panel.removeClass('open').addClass('closed');
-            this.$explore_panel.animate({height: "0px"});
-            this.$explore.css("bottom", "");
+            this.$explore_panel.animate({bottom: "-215px", opacity: 0}, {duration: 800, queue: false});
+
 
             var _this = this;
             setTimeout(function() {
                 _this.eiv.resize();
             }, 1000);
-            
+
             ga('send', { 'hitType': 'event', 'eventCategory': 'panel', 'eventAction': 'close', 'eventLabel': this.experience.get('name') });
         },
 
@@ -77,8 +80,8 @@ var app = app || {};
                     var extn = k.src.match(/\.\w+$/);
                     var cache_img = 'http://xola.com/experiences/' + that.className + '/medias/' + k.id + "?width=260&height=200";
                     var panel_html = compiled_template({
-                        exp_id: that.className, 
-                        img_id: k.id, 
+                        exp_id: that.className,
+                        img_id: k.id,
                         original_url: k.src,
                         panel_img: cache_img,
                         caption: k.caption || ''
@@ -86,7 +89,7 @@ var app = app || {};
                     $('.img-container').append(panel_html);
                 }
             });
-            
+
             ga('send', { 'hitType': 'event', 'eventCategory': 'panel', 'eventAction': 'load_photos', 'eventLabel': this.experience.get('name') });
         },
 
@@ -100,16 +103,12 @@ var app = app || {};
                 $("#explore_panel .explore_panel_container .img-container").width(totalWidth * 1.5);
             }
 
-            if (this.$explore_panel.height() > 0) {
-                this.$explore.css("bottom", this.$explore_panel.height());
-            }
-
             ga('send', { 'hitType': 'event', 'eventCategory': 'panel', 'eventAction': 'show_photos', 'eventLabel': this.experience.get('name') });
         },
 
         img_click: function(e) {
             // console.info(e.currentTarget.src, e.currentTarget.id);
-            
+
             var elem = $("#" + e.currentTarget.id);
             var large_img = "http://xola.com" + elem.attr('data-original-url');
             var large_img_caption = elem.attr('title');
