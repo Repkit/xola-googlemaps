26605cda5803fd3b4db1789a562cfc142e4573aa
Rushi Vishavadia <rushi.v@gmail.com>
2013-07-03 17:58:33 +0400

    Add JSHint and ScriptLinker

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     concat: {
       options: {
         stripBanners: true,
-        banner: '/*! Grunt Processed <%= grunt.task.current.filesSrc.join(";") %> on <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
+        banner: '/*! Grunt Processed [<%= grunt.task.current.filesSrc.join(",") %>] on <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
       },
       vendor: {
         src: "<%= vendor_src %>",
@@ -21,34 +21,80 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '/*! Minified <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! Minified [<%= grunt.task.current.filesSrc.join(",") %>] on <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
       },
       vendor: {
-        src: 'build/vendor*.js',
-        dest: 'build/vendor<%= grunt.template.today("yyyymmdd") %>.min.js'
+        src: "<%= vendor_src %>",
+        dest: 'build/<%= vendor_dest_filename %>',
+        nonull: true
       },
       app: {
-        src: 'build/app*.js',
-        dest: 'build/app<%= grunt.template.today("yyyymmdd") %>.min.js'
+        src: "<%= app_src %>",
+        dest: 'build/<%= app_dest_filename %>',
+        nonull: true
       }
     },
+    jshint: {
+      // jshint options http://www.jshint.com/docs/
+      options: {
+        "browser": true,
+        "undef": true,
+        "unused": true,
+        "curly": true,
+        "globals": {"Backbone": false, "google" : false, "ga": false, "_": false, "OverlappingMarkerSpiderfier" : false, "xola" : false},
+        "eqeqeq": true,
+        "immed": true,
+        "latedef": true,
+        "newcap": true,
+        "undef": true,
+        "unused": true,
+        "devel": true,
+        "jquery": true
+      },
+      app: "<%= app_src %>"
+    },
 
-    // Properties
+    scriptlinker: {
+      vendor: {
+        options: {
+          startTag: "<!--VENDOR_SCRIPTS-->\n",
+          endTag: "<!--VENDOR_SCRIPTS_END-->",
+          fileTmpl: "<script src='%s'></script>\n",
+        },
+        files: {
+          // Target-specific file lists and/or options go here.
+          'sample.html': "build/<%= vendor_dest_filename %>"
+        },
+      },
+      app: {
+        options: {
+          startTag: "<!--APP_SCRIPTS-->\n",
+          endTag: "<!--APP_SCRIPTS_END-->",
+          fileTmpl: "<script src='%s'></script>\n",
+        },
+        files: {
+          // Target-specific file lists and/or options go here.
+          'sample.html': "build/<%= app_dest_filename %>"
+        },
+      },
+    },
+
+    // Vendor Library Definitions
     vendor_src: ['js/lib/jquery*.js', 'js/lib/underscore*.js', 'js/lib/backbone*.js', 'js/lib/oms*', 'js/lib/jqueryui/jquery-ui-1.10.3.custom.min.js'],
-    app_src: ['js/app/models/*.js', 'js/app/collections/*', 'js/app/views/*.js', 'js/app/router.js', 'js/app/app.js']
+    // Application Code
+    app_src: ['js/app/models/*.js', 'js/app/collections/*', 'js/app/views/*.js', 'js/app/router.js', 'js/app/app.js'],
+    // Destination filename for minified vendor library
+    vendor_dest_filename: 'vendor<%= grunt.template.today("yyyymmdd") %>.min.js',
+    // Destination filename for minified application code
+    app_dest_filename: 'app<%= grunt.template.today("yyyymmdd") %>.min.js'
   });
 
-  // A very basic default task.
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
-  });
-
-  // Load the plugin that provides the "concat" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify'); // will minify
+  grunt.loadNpmTasks('grunt-contrib-jshint'); // will minify
+  grunt.loadNpmTasks('grunt-scriptlinker');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'scriptlinker']);
 
 };
