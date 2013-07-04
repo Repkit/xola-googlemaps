8dedbd7dac6ea977bf50bd67caf7e42320e7f429
Rushi Vishavadia <rushi.v@gmail.com>
2013-07-04 16:57:36 +0400

    Add Watch and Clean modules.
    Basic cleanup and documentation of the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,17 @@
 module.exports = function(grunt) {
 
+  // The plugins we're using 
+  grunt.loadNpmTasks('grunt-contrib-concat'); // Concatenate files
+  grunt.loadNpmTasks('grunt-contrib-uglify'); // Minify JavaScript
+  grunt.loadNpmTasks('grunt-contrib-jshint'); // Run JSHint
+  grunt.loadNpmTasks('grunt-scriptlinker'); // Insert JS Script tags in an HTML file
+  grunt.loadNpmTasks('grunt-contrib-watch'); // Watch for changes to files and execute tasks
+  grunt.loadNpmTasks('grunt-contrib-clean'); // Clean up things
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
       options: {
         stripBanners: true,
@@ -10,12 +19,12 @@ module.exports = function(grunt) {
       },
       vendor: {
         src: "<%= vendor_src %>",
-        dest: 'build/vendor.js',
+        dest: 'cache/vendor.js',
         nonull: true
       },
       app: {
         src: "<%= app_src %>",
-        dest: 'build/app.js',
+        dest: 'cache/app.js',
         nonull: true
       },
     },
@@ -26,12 +35,12 @@ module.exports = function(grunt) {
       },
       vendor: {
         src: "<%= vendor_src %>",
-        dest: 'build/<%= vendor_dest_filename %>',
+        dest: 'cache/<%= vendor_dest_filename %>',
         nonull: true
       },
       app: {
         src: "<%= app_src %>",
-        dest: 'build/<%= app_dest_filename %>',
+        dest: 'cache/<%= app_dest_filename %>',
         nonull: true
       }
     },
@@ -61,27 +70,38 @@ module.exports = function(grunt) {
         options: {
           startTag: "<!--VENDOR_SCRIPTS-->\n",
           endTag: "<!--VENDOR_SCRIPTS_END-->",
-          fileTmpl: "<script src='%s'></script>\n",
+          fileTmpl: "<script src='%s?r=<%= grunt.template.today(\"yyyymmddHHMMss\") %>'></script>\n",
         },
         files: {
-          'sample.html': "build/<%= vendor_dest_filename %>"
+          'sample.html': "cache/<%= vendor_dest_filename %>"
         },
       },
       app: {
         options: {
           startTag: "<!--APP_SCRIPTS-->\n",
           endTag: "<!--APP_SCRIPTS_END-->",
-          fileTmpl: "<script src='%s'></script>\n",
+          fileTmpl: "<script src='%s?r=<%= grunt.template.today(\"yyyymmddHHMMss\") %>'></script>\n",
         },
         files: {
-          'sample.html': "build/<%= app_dest_filename %>"
+          'sample.html': "cache/<%= app_dest_filename %>"
         },
       },
     },
 
-    // Vendor Library Definitions
-    vendor_src: ['js/lib/jquery*.js', 'js/lib/underscore*.js', 'js/lib/backbone*.js', 'js/lib/oms*', 'js/lib/jqueryui/jquery-ui-1.10.3.custom.min.js'],
-    // Application Code
+    watch: {
+      vendor: {
+        files: "<%= app_src %>",
+        tasks: ['quickwatch-vendor']
+      },
+      app: {
+        files: "<%= app_src %>",
+        tasks: ['quickwatch-app']
+      }
+    },
+
+    // Vendor Library Definitions, in order
+    vendor_src: ['js/lib/jquery*.js', 'js/lib/underscore*.js', 'js/lib/backbone*.js', 'js/lib/oms*', 'js/lib/jqueryui/jquery-ui-1.10.3.custom.min.js', 'js/lib/xola*'],
+    // Application Code, in order
     app_src: ['js/app/models/*.js', 'js/app/collections/*', 'js/app/views/*.js', 'js/app/router.js', 'js/app/app.js'],
     // Destination filename for minified vendor library
     vendor_dest_filename: 'vendor<%= grunt.template.today("yyyymmdd") %>.min.js',
@@ -89,12 +109,15 @@ module.exports = function(grunt) {
     app_dest_filename: 'app<%= grunt.template.today("yyyymmdd") %>.min.js'
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify'); // will minify
-  grunt.loadNpmTasks('grunt-contrib-jshint'); // will minify
-  grunt.loadNpmTasks('grunt-scriptlinker');
+  grunt.event.on('watch', function(action, filepath, target) {
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+  });
 
   // Default task(s).
   grunt.registerTask('default', ['jshint', 'uglify', 'scriptlinker']);
+  // This task will just uglify and run scriptlinker for the app code only
+  grunt.registerTask('quickwatch-app', ['uglify:app', 'scriptlinker:app']);
+  // This task will just uglify and run scriptlinker for the vendor code only
+  grunt.registerTask('quickwatch-vendor', ['uglify:vendor', 'scriptlinker:vendor']);
 
 };
